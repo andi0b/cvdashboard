e63535a7b4df7c92fcd62985bc33534b03ce4684
andi0b <andix25@gmail.com>
2020-10-27 21:31:29 +0100

    refactored calculate_deltas function and apply them with a lateral join

--- db/01_create_functions.sql
+++ db/01_create_functions.sql
@@ -1,4 +1,5 @@
 drop function if exists inzidenz(days integer, per_capita integer, delta_days integer);
+
 create or replace function inzidenz(days int default 7, per_capita int default 100000, delta_days int default 5)
     returns table
             (
@@ -8,44 +9,62 @@ create or replace function inzidenz(days int default 7, per_capita int default 1
                 typ                             varchar,
                 anz_faelle                      int,
                 inzidenz                        float,
-                inzidenz_days_ago               float,
                 inzidenz_delta                  float,
                 inzidenz_delta_daily            float,
                 inzidenz_increase_percent       float,
                 inzidenz_increase_daily_percent float
             )
-    language SQL
+    language sql
 as
 $$
-select *,
-       (pow(1 + abs((inzidenz_increase_percent / 100.0)), 1.0 / delta_days) - 1) * 100
-           * (case when inzidenz_increase_percent < 0 then -1 else 1 end)
-           as inzidenz_increase_daily_percent
-
+select date, region, rid, typ, anz_faelle, inzidenz, deltas.*
 from (
-         select *,
-                inzidenz_delta / delta_days as inzidenz_delta_daily,
-                case
-                    when inzidenz_days_ago = 0
-                        then 0
-                    else
-                        inzidenz_delta / inzidenz_days_ago * 100
-                    end                     as inzidenz_increase_percent
-
+         select *, lag(inzidenz, delta_days) over w_delta as inzidenz_days_ago
          from (
-                  select *,
-                         lag(inzidenz, delta_days) over w_delta              as inzidenz_days_ago,
-                         inzidenz - (lag(inzidenz, delta_days) over w_delta) as inzidenz_delta
-                  from (
-                           select date,
-                                  region,
-                                  rid,
-                                  typ,
-                                  sum(anz_faelle) over w_inz                                       as anz_faelle,
-                                  (sum(anz_faelle) over w_inz)::float / anz_einwohner * per_capita as inzidenz
-                           from timeline_full
-                               window w_inz as (partition by region order by date rows between days preceding and 1 preceding)) inz
-                      window w_delta as (partition by region order by date)
-              ) delta
-     ) deltapercent
+                  select date,
+                         region,
+                         rid,
+                         typ,
+                         sum(anz_faelle) over w_inz                                       as anz_faelle,
+                         (sum(anz_faelle) over w_inz)::float / anz_einwohner * per_capita as inzidenz
+                  from timeline_full
+                      window w_inz as (partition by region order by date rows between days preceding and 1 preceding)
+              ) inz
+             window w_delta as (partition by region order by date)
+     ) days_ago,
+     calculate_deltas(inzidenz, inzidenz_days_ago, delta_days) deltas
 $$;
+
+
+create or replace function calculate_deltas(value_current float, value_days_ago float, delta_days int default 1,
+                                            out delta float,
+                                            out delta_daily float,
+                                            out increase_percent float,
+                                            out increase_percent_daily float)
+    language plpgsql
+as
+$$
+declare
+    increase_ratio       float;
+    increase_ratio_daily float;
+begin
+    delta := value_current - value_days_ago;
+    delta_daily := delta / delta_days;
+
+    increase_ratio := case
+                          when value_days_ago = 0 then 0
+                          else delta / value_days_ago end;
+
+    -- calculate the daily ratio. applying it delta_days times,
+    -- will result in the same as applying increase_ratio once
+    increase_ratio_daily :=
+                (pow(1 + abs(increase_ratio), 1.0 / delta_days) - 1)
+                * (case when increase_ratio < 0 then -1 else 1 end);
+
+    increase_percent := increase_ratio * 100;
+    increase_percent_daily := increase_ratio_daily * 100;
+end;
+$$;
+
+
+select * from inzidenz2() where region = 'Österreich'
