ea5212237f66e89b25b09b6a3bf831562fe7fe9d
andi0b <andix25@gmail.com>
2020-10-28 13:23:36 +0100

    reorder functions to make them work on uninitialized db

--- db/01_create_functions.sql
+++ db/01_create_functions.sql
@@ -1,3 +1,33 @@
+create or replace function calculate_deltas(value_current float, value_days_ago float, delta_days int default 1,
+                                            out delta float,
+                                            out delta_daily float,
+                                            out increase_percent float,
+                                            out increase_percent_daily float)
+    language plpgsql
+as
+$$
+declare
+    increase_ratio       float;
+    increase_ratio_daily float;
+begin
+    delta := value_current - value_days_ago;
+    delta_daily := delta / delta_days;
+
+    increase_ratio := case
+                          when value_days_ago = 0 then 0
+                          else delta / value_days_ago end;
+
+    -- calculate the daily ratio. applying it delta_days times,
+    -- will result in the same as applying increase_ratio once
+    increase_ratio_daily :=
+                (pow(1 + abs(increase_ratio), 1.0 / delta_days) - 1)
+                * (case when increase_ratio < 0 then -1 else 1 end);
+
+    increase_percent := increase_ratio * 100;
+    increase_percent_daily := increase_ratio_daily * 100;
+end;
+$$;
+
 drop function if exists inzidenz(days integer, per_capita integer, delta_days integer);
 create or replace function inzidenz(days int default 7, per_capita int default 100000, delta_days int default 5)
     returns table
@@ -59,35 +89,4 @@ from (
              window w_current_period as (partition by region order by date rows delta_days - 1 preceding),
                  w_previous_period as (partition by region order by date rows between delta_days * 2 - 1 preceding and delta_days preceding)) a,
     calculate_deltas(faelle_current_period, faelle_previous_period, delta_days) deltas
-$$;
-
-
-create or replace function calculate_deltas(value_current float, value_days_ago float, delta_days int default 1,
-                                            out delta float,
-                                            out delta_daily float,
-                                            out increase_percent float,
-                                            out increase_percent_daily float)
-    language plpgsql
-as
-$$
-declare
-    increase_ratio       float;
-    increase_ratio_daily float;
-begin
-    delta := value_current - value_days_ago;
-    delta_daily := delta / delta_days;
-
-    increase_ratio := case
-                          when value_days_ago = 0 then 0
-                          else delta / value_days_ago end;
-
-    -- calculate the daily ratio. applying it delta_days times,
-    -- will result in the same as applying increase_ratio once
-    increase_ratio_daily :=
-                (pow(1 + abs(increase_ratio), 1.0 / delta_days) - 1)
-                * (case when increase_ratio < 0 then -1 else 1 end);
-
-    increase_percent := increase_ratio * 100;
-    increase_percent_daily := increase_ratio_daily * 100;
-end;
 $$;
