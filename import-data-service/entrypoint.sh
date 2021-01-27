4575652a3bc3643557449d53c4928021384dd2d3
andi0b <andix25@gmail.com>
2021-01-27 14:15:42 +0100

    home dashboard

--- import-data-service/entrypoint.sh
+++ import-data-service/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-sh /scripts/download-data.sh && sh /scripts/init-db.sh
-
 echo "initial db import, waiting 30s until DB is up"
 echo
 
 sleep 30
+
+sh /scripts/download-data.sh && sh /scripts/init-db.sh
 
 echo
 echo "db import finished, starting cron"
